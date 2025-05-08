c8ee193019b37f4dd16dd866fcb4f20cb4b4fe54
OLVCORE <olvsistemas@olvinternacional.com.br>
2025-05-08 15:13:09 -0300

    Delete script.js
    Troca dos layouts para arquivos com navegão e conteudo atualizado

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function toggleTheme() {
-    const body = document.body;
-    body.classList.toggle('dark');
-    body.classList.toggle('light');
-}
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
